762e910f96f541e4d18598f18d3daa7c3bc33387
Simon Elsbrock <simon@iodev.org>
2012-07-15 02:26:58 +0200

    automatically reload tumblr images every ten minutes

--- scripts/tumblr.js
+++ scripts/tumblr.js
@@ -1,25 +1,10 @@
 $(document).ready(function() {
-	(function() {
-		var blabber_show = 5;
-		var blabb0r = function() {
-			if (blabber_show-- == 0) {
-				tumblr_img();
-				return;
-			}
-			/* lulz */
-			$('#tumblr img').fadeOut('slow', function() {
-				$('#tumblr img').fadeIn('slow', blabb0r());
-			});
-		}
-		blabb0r();
-	})();
-
 	var current = 0;
+    var image_loaded = false;
 	var tumblr_img; tumblr_img = function() {
 		var max_h = 160;
 		var max_w = 250;
 		var resize = function(img) {
-			console.log("w: "+$(img).width()+" h: "+$(img).height());
 			if ($(img).height() > max_h) {
 				var h = max_h;
 				var w = Math.ceil($(img).width() / $(img).height() * max_h);
@@ -42,6 +27,30 @@ $(document).ready(function() {
 				current = ++current % tumblr_api_read.posts.length;
 				$('#tumblr img').replaceWith(t).fadeIn('slow');
 				setTimeout(function() { tumblr_img() }, 10000);
+                image_loaded = 1;
 			});
 	};
+
+	(function() {
+		var blabb0r = function() {
+			if (image_loaded == true) return;
+
+			/* lulz */
+			$('#tumblr img').fadeOut('slow', function() {
+				$('#tumblr img').fadeIn('slow', blabb0r());
+			});
+		}
+        tumblr_img();
+		blabb0r();
+	})();
+
+    setInterval(function() {
+        $.getScript('http://raumzeitlabor.tumblr.com/api/read/json?number=1&type=photo', function(data, textStatus, jqxhr) {
+            if (jqxhr.status == 200) {
+                current = 0;
+            } else {
+                console.log("tumblr reload failed");
+            }
+        });
+    }, 1000*60*10);
 });
