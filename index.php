89cd285b2bfa5573c6bb44b36709de009e3b6eaf
Severin Schols <severin@schols.de>
2012-11-16 18:38:15 +0100

    Fixing issues introduced by VRN widget update in 5b8b91236a0b8757d0d6867f47a4aa5b055e41f2

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
 						<div class="row">
 							<div class="span5 centered box" id="frame_wrapper">
 								<h4>RNV: Boveristra&szlig;e</h4>
-								<iframe src="http://efa9-5.vrn.de/dm_rbl/XSLT_DM_REQUEST?language=de&itdLPxx_dmlayout=gadget&itdLPxx_gadget=version_1.0.5&timeOffset=3&type_dm=any&mode=direct&limit=14&useRealtime=1&locationServerActive=1&anySigWhenPerfectNoOtherMatches=1&anyHitListReductionLimit=40&anyMaxSizeHitList=550&name_dm=6002359" id="frame"></iframe>
+								<iframe src="http://efa9-5.vrn.de/vrn/XSLT_DM_REQUEST?language=de&itdLPxx_dmlayout=gadget&itdLPxx_gadget=version_1.0.5&timeOffset=3&type_dm=any&mode=direct&limit=8&useRealtime=1&locationServerActive=1&anySigWhenPerfectNoOtherMatches=1&anyHitListReductionLimit=40&anyMaxSizeHitList=550&name_dm=6002359" id="frame"></iframe>
 							</div>
 							<div class="span4 centered box">
 								<h4>log.raumzeitlabor.de</h4>
