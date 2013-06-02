f4cbf1bfcd160dc3f30f93fa26603c770b4c65a4
Severin Schols <severin@schols.de>
2013-06-02 11:12:17 +0200

    Space API

--- ajax.php
+++ ajax.php
@@ -2,69 +2,74 @@
 include("PachubeAPI/PachubeAPI.php");
 include("config.php");
 
-$output= "";
-
-// Try to see if there's some recent cached output available
-if (file_exists($config->cache) && (time() < filemtime($config->cache) + $config->cache_expire)) {
-
-	// It is. Let's use that as our output.
-	$output = file_get_contents($config->cache);
-
-} else {
-
-	// No recent cached output. We need to get it from Cosm
-	$pachube = new PachubeAPI($config->api_key);
-
-	// Get the feed from the Cosm API, as a json
-	$json = $pachube->getFeed("json", $config->feed);
-
-	$data = array();
+function getInfoscreenData() {
+	global $config;
 	
-	// iterate through all data streams to get the values we need
-	foreach (json_decode($json)->datastreams as $datastream) {
-		switch ($datastream->id) {
-			case $config->temperature:
-				$data["temperature"] = $datastream->current_value;
-				break;
-			case $config->power:
-				$data["power"] = $datastream->current_value;
-				break;
-			case $config->door:
-				$data["door"] = $datastream->current_value;
-				break;
-			case $config->devices:
-				$data["devices"] = $datastream->current_value;
-				break;
-			case $config->members:
-				$data["members"] = $datastream->current_value;
-				$data["memberratio"] = "" . (round(($data["members"]/314931)*100,3));
-				break;
-			case $config->account:
-				$data["account"] = $datastream->current_value;
-				break;
-			case $config->internet_up:
-				$data["internet_up"] = $datastream->current_value;
-				break;
-			case $config->internet_down:
-				$data["internet_down"] = $datastream->current_value;
-				break;
+	// Try to see if there's some recent cached output available
+	if (file_exists($config->cache) && (time() < filemtime($config->cache) + $config->cache_expire)) {
+
+		// It is. Let's use that as our output.
+		return json_decode(file_get_contents($config->cache),true);
+
+	} else {
+
+		// No recent cached output. We need to get it from Cosm
+		$pachube = new PachubeAPI($config->api_key);
+
+		// Get the feed from the Cosm API, as a json
+		$json = $pachube->getFeed("json", $config->feed);
+
+		$data = array();
+		
+		// iterate through all data streams to get the values we need
+		foreach (json_decode($json)->datastreams as $datastream) {
+			switch ($datastream->id) {
+				case $config->temperature:
+					$data["temperature"] = $datastream->current_value;
+					break;
+				case $config->power:
+					$data["power"] = $datastream->current_value;
+					break;
+				case $config->door:
+					$data["door"] = $datastream->current_value;
+					break;
+				case $config->devices:
+					$data["devices"] = $datastream->current_value;
+					break;
+				case $config->members:
+					$data["members"] = $datastream->current_value;
+					$data["memberratio"] = "" . (round(($data["members"]/314931)*100,3));
+					break;
+				case $config->account:
+					$data["account"] = $datastream->current_value;
+					break;
+				case $config->internet_up:
+					$data["internet_up"] = $datastream->current_value;
+					break;
+				case $config->internet_down:
+					$data["internet_down"] = $datastream->current_value;
+					break;
+			}
 		}
+		
+		// Write to cache
+		$data["source"] = "cache";
+		file_put_contents($config->cache, json_encode($data));
+		
+		// Return live output
+		$data["source"] = "live";
+		
+		return $data;		
 	}
-	
-	// Generate output
-	$data["source"] = "live";
-	$output = json_encode($data);
-	
-	// Write to cache
-	$data["source"] = "cache";
-	file_put_contents($config->cache, json_encode($data));
 }
 
-// Send header
-header('Cache-Control: no-cache, must-revalidate');
-header('Expires: Mon, 19 Jul 1997 00:00:00 GMT');
-header('Content-type: application/json');
+if(realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
+	// Send header
+	header('Cache-Control: no-cache, must-revalidate');
+	header('Expires: Mon, 19 Jul 1997 00:00:00 GMT');
+	header('Content-type: application/json');
 
-// Send output
-echo $output;
+	// Send output
+	echo json_encode(getInfoscreenData());
+}
 ?>
---
+++ spaceapi.php
@@ -0,0 +1,60 @@
+<?php
+include("ajax.php");
+$data = getInfoscreenData();
+
+// Send header
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 19 Jul 1997 00:00:00 GMT');
+header('Content-type: application/json; charset=utf-8');
+
+$data = array(
+	"api" => "0.13",
+	"space" => "RaumZeitLabor",
+	"logo" => "http://raumzeitlabor.de/w/images/8/85/RaumZeitLabor_-_Logo_-_Schwarz.png",
+	"url" => "http://raumzeitlabor.de",
+	"location" => array(
+		"address" => "Boveristrasse 22-24, 68309 Mannheim, Germany",
+		"lon" => 8.4994033,
+		"lat" => 49.5079481
+	),
+	"spacefed-auth" => false,
+	"contact" => array(
+		"email" => "info@raumzeitlabor.de",
+		"irc" => "irc://irc.hackint.eu/raumzeitlabor",
+		"ml" => "raumzeitlabor@raumzeitlabor.de",
+		"twitter" => "@RaumZeitLabor",
+		"phone" => "+4962176231370",
+		"issue-mail" => "aGVsbG9AdGllZnB1bmt0LmNvbQ=="
+	),
+	"issue-report-channels" => array("issue-mail"),
+	"sensors" => array(
+		array( "temp" => array(
+			"Main Space" => $data["temperature"] . "C"
+		)),
+		array( "power_consumption" => array(
+			"Main" => $data["power"] . "W"
+		)),
+		array( "member_count" => array( "member_count" => $data["members"])),
+		array( "devices" => array( "main" => $data["devices"])) ,
+		array( "account_balance" => array( "main" => $data["account"])) ,
+	),
+	"cache" => array( "schedule" => "m.02" ),
+	"state" => array(
+		"icon" => array(
+			"open" => "http://status.raumzeitlabor.de/images/green.png",
+			"closed" => "http://status.raumzeitlabor.de/images/red.png"
+		),
+		"open" => ($data["door"] == 1)
+	),
+	"projects" => array(
+		"http://github.com/raumzeitlabor",
+		"http://wiki.raumzeitlabor.de/wiki/Kategorie:Projekt"
+	)
+);
+
+echo json_encode($data);
+?>
+
+	
+	
+	
