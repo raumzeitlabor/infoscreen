4ddb31c10afc78ec62dc619e09c17501a7f97548
Severin Schols <severin@schols.de>
2014-03-24 18:52:55 +0100

    fixed graph refresh

--- scripts/main.js
+++ scripts/main.js
@@ -32,7 +32,7 @@ function updateData() {
 }
 
 function updateGraph() {
-	$('#graph').attr('src', function ( i, val ) { return graph_source + "&v=" + new Date().getTime()});
+	$('#graph').attr('src', function ( i, val ) { return graph_source + "?v=" + new Date().getTime()});
 	setTimeout('updateGraph()',120000);
 }
 
