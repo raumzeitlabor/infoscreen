6b3b27235c8fc6352fb196807ed51d4d97ed0c65
Felicitus <felicitus@felicitus.org>
2014-06-08 17:02:44 +0200

    Replaced the cosm api with the xively api. This gives SSL errors because they're delivering the cert for api.xively.com at api.cosm.com *sigh*

--- graph.php
+++ graph.php
@@ -4,16 +4,16 @@ function loadGraph()
 	$type = $_GET['type'];
 	switch ($type) {
 		case 'stromverbrauch':
-			$imgname = "https://api.cosm.com/v2/feeds/42055/datastreams/Strom_Leistung.png?width=500&height=200&c=ff33ff&duration=30minutes&title=Stromverbrauch&stroke_size=2&show_axis_labels=true&detailed_grid=true&scale=auto&timezone=Berlin&v=1395676268666";
+			$imgname = "https://api.xively.com/v2/feeds/42055/datastreams/Strom_Leistung.png?width=500&height=200&c=ff33ff&duration=30minutes&title=Stromverbrauch&stroke_size=2&show_axis_labels=true&detailed_grid=true&scale=auto&timezone=Berlin&v=1395676268666";
 			break;
 		case 'temperatur':
-			$imgname = "https://api.cosm.com/v2/feeds/42055/datastreams/Temperatur_Raum_Beamerplattform.png?width=500&height=200&c=ff33ff&duration=3hours&title=Temperatur&stroke_size=2&show_axis_labels=true&detailed_grid=true&scale=auto&timezone=Berlin&v=1395676268666";
+			$imgname = "https://api.xively.com/v2/feeds/42055/datastreams/Temperatur_Raum_Beamerplattform.png?width=500&height=200&c=ff33ff&duration=3hours&title=Temperatur&stroke_size=2&show_axis_labels=true&detailed_grid=true&scale=auto&timezone=Berlin&v=1395676268666";
 			break;
 		case 'payback':
-			$imgname = "https://api.cosm.com/v2/feeds/42055/datastreams/Payback_Punkte.png?width=500&height=200&c=ff33ff&duration=30days&title=Payback%20Punkte&stroke_size=2&show_axis_labels=true&detailed_grid=true&scale=auto&timezone=Berlin&v=1395676268666";
+			$imgname = "https://api.xively.com/v2/feeds/42055/datastreams/Payback_Punkte.png?width=500&height=200&c=ff33ff&duration=30days&title=Payback%20Punkte&stroke_size=2&show_axis_labels=true&detailed_grid=true&scale=auto&timezone=Berlin&v=1395676268666";
 			break;
 		case 'kontostand':
-			$imgname = "https://api.cosm.com/v2/feeds/42055/datastreams/Kontostand.png?width=500&height=200&c=ff33ff&duration=30days&title=Kontostand&stroke_size=2&show_axis_labels=true&detailed_grid=true&scale=auto&timezone=Berlin&v=1395676268666";
+			$imgname = "https://api.xively.com/v2/feeds/42055/datastreams/Kontostand.png?width=500&height=200&c=ff33ff&duration=30days&title=Kontostand&stroke_size=2&show_axis_labels=true&detailed_grid=true&scale=auto&timezone=Berlin&v=1395676268666";
 			break;
 	}
     $im = @imagecreatefrompng($imgname);
